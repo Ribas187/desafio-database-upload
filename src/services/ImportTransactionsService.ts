8278e3b57c35d7c8797e289824aae6be6e45e754
Ribas187 <guilherme.ribas.tech@gmail.com>
2020-04-23 12:19:15 -0300

    fixes

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -4,6 +4,8 @@ import fs from 'fs';
 import { getRepository, getCustomRepository } from 'typeorm';
 import uploadConfig from '../config/upload';
 
+import AppError from '../errors/AppError';
+
 import Transaction from '../models/Transaction';
 import Category from '../models/Category';
 import TransactionsRepository from '../repositories/TransactionsRepository';
@@ -16,6 +18,10 @@ interface TransactionCSV {
   title: string;
   value: number;
   type: 'income' | 'outcome';
+  category: string;
+}
+
+interface NewCategory {
   category: string;
 }
 
@@ -32,35 +38,65 @@ class ImportTransactionsService {
     const parseCSV = csvReadStream.pipe(parsers);
 
     const transactionsCSV: TransactionCSV[] = [];
+    const categories: string[] = [];
 
     parseCSV.on('data', async line => {
       const [title, type, value, category] = line;
 
+      if (!title || !type || !value) throw new AppError('File content invalid');
+
       transactionsCSV.push({ title, type, value, category });
+      categories.push(category);
     });
 
     await new Promise(resolve => parseCSV.on('end', resolve));
 
-    const categories = await Promise.all(
-      transactionsCSV.map(async ({ category }) => {
-        const categoryExists = await categoriesRepository.findOne({
-          where: {
-            title: category,
-          },
-        });
+    const newCategories = (
+      await Promise.all(
+        categories.map(async category => {
+          const categoryExists = await categoriesRepository.findOne({
+            where: {
+              title: category,
+            },
+          });
 
-        if (categoryExists) {
-          const category_id = categoryExists.id;
+          if (categoryExists) {
+            return '';
+          }
 
-          return category_id;
-        }
+          return category;
+        }),
+      )
+    )
+      .filter(category => category !== '')
+      .filter((a, b) => categories.indexOf(a) === b);
 
+    const newCategoriesAll = await Promise.all(
+      newCategories.map(async category => {
         const newCategory = categoriesRepository.create({
           title: category,
         });
-        const category_id = newCategory.id;
 
         await categoriesRepository.save(newCategory);
+        return newCategory;
+      }),
+    );
+
+    const categories_id = await Promise.all(
+      categories.map(async category => {
+        if (!newCategories.includes(category)) {
+          const categoryT = await categoriesRepository.findOne({
+            where: { title: category },
+          });
+
+          return categoryT?.id;
+        }
+
+        const newCategory = newCategoriesAll.find(
+          categ => categ.title === category,
+        );
+
+        const category_id = newCategory?.id;
 
         return category_id;
       }),
@@ -74,7 +110,7 @@ class ImportTransactionsService {
           title,
           type,
           value,
-          category_id: categories[index],
+          category_id: categories_id[index],
         });
       }),
     );
